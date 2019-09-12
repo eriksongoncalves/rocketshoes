c85f85913a8f5e25905e8c2a0e923a0b05a9f96c
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2019-09-12 16:34:14 -0300

    Configuração Reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 
+import './config/Reactotron';
+
 import store from './store';
 import Routes from './routes';
 
