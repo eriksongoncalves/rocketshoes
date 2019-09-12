0c479694489b629f5f9a0add63d2da7ca2e2f663
Erikson <eriksongoncalves@yahoo.com.br>
2019-09-12 17:57:07 -0300

    Refatorando reducer e actions

--- frontend/src/pages/Cart/index.js
+++ frontend/src/pages/Cart/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect, bindActionCreators } from 'redux';
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
+import * as CartActions from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart, total, removeToCart, updateAmount }) {
+    function increment(id) {
+        updateAmount(product.id, product.amount + 1);
+    }
+
+    function decrement(id) {
+        updateAmount(product.id, product.amount - 1);
+    }
+
+
+
     return (
         <Container>
             <ProductTable>
@@ -17,26 +30,28 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><img src="https://static.netshoes.com.br/produtos/tenis-puma-wired/74/D14-5566-174/D14-5566-174_detalhe1.jpg" alt="" /></td>
-                        <td>
-                            <strong>Tênis</strong>
-                            <span>R$129,90</span>
-                        </td>
-                        <td>
-                            <div>
-                                <button>
-                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
-                                </button>
-                                <input type="number" readOnly value={2} />
-                                <button>
-                                    <MdAddCircleOutline size={20} color="#7159c1" />
-                                </button>
-                            </div>
-                        </td>
-                        <td><strong>R$ 258,80</strong></td>
-                        <td><button><MdDelete size={20} color="#7159c1" /></button></td>
-                    </tr>
+                    {cart.map(product => (
+                        <tr>
+                            <td><img src={product.image} alt={product.title} /></td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button onClick={() => decrement(product.id)}>
+                                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                    <input type="number" readOnly value={product.amount} />
+                                    <button onClick={() => increment(product.id)}>
+                                        <MdAddCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                </div>
+                            </td>
+                            <td><strong>{product.subtotal}</strong></td>
+                            <td><button onClick={() => removeToCart(product.id)}><MdDelete size={20} color="#7159c1" /></button></td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
 
@@ -45,9 +60,25 @@ export default function Cart() {
 
                 <Total>
                     <span>TOTAL</span>
-                    <strong>R$1920,28</strong>
+                    <strong>{total}</strong>
                 </Total>
             </footer>
         </Container>
     )
 }
+
+const mapStateToProps = state => ({
+    cart: state.cart.map(product => ({
+        ...product,
+        subtotal: formatPrice(product.price * product.amount)
+    })),
+    total: formatPrice(
+        state.cart.reduce((total, product) => {
+            return total + product.price * product.amount
+        }, 0)
+    )
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
@@ -34,17 +37,13 @@ class Home extends Component {
     }
 
     handleAddProduct = product => {
-        const { dispatch } = this.props;
-
-        dispatch({
-            type: 'ADD_TO_CART',
-            product
-        })
-
+        const { addToCart } = this.props;
+        addToCart(product);
     }
 
     render() {
         const { products } = this.state;
+        const { amount } = this.props;
         return (
             <ProductList>
                 {
@@ -55,7 +54,7 @@ class Home extends Component {
                             <span>{product.priceFormatted}</span>
                             <button onClick={() => this.handleAddProduct(product)}>
                                 <div>
-                                    <MdAddShoppingCart size={16} color="#fff" /> 3
+                                    <MdAddShoppingCart size={16} color="#fff" /> {amount[product.id] || 0}
                                 </div>
                                 <span>ADICIONAR AO CARRINHO</span>
                             </button>
@@ -66,5 +65,12 @@ class Home extends Component {
         )
     }
 }
+const mapStateToProps = state => ({
+    amount: state.cart.reduce((amount, product) => {
+        amount[product.id] = product.amount;
+        return amount;
+    }, {})
+});
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch);
 
-export default connect()(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
